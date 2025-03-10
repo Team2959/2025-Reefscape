e34cd2ac11fe15a756bd905082fee3258a89dc27
CW Tech <cwtech2959@gmail.com>
2025-03-10 19:54:41 -0400

    Making Pubs and Subs to update April Tag PID

--- src/main/java/frc/robot/cwtech/AprilTagPID.java
+++ src/main/java/frc/robot/cwtech/AprilTagPID.java
@@ -5,7 +5,11 @@
 package frc.robot.cwtech;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.BooleanSubscriber;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.LimelightHelpers;
 
@@ -30,38 +34,70 @@ public class AprilTagPID
     PIDController m_xSpeedController = new PIDController(kSpeedKp, kSpeedKi, kSpeedKd);
     PIDController m_zSpeedController = new PIDController(kSpeedKp, kSpeedKi, kSpeedKd);
 
+    private final DoubleSubscriber m_kPSpeedSub;
+    private final DoubleSubscriber m_kPRotationSub;
+    private final DoubleSubscriber m_kISpeedSub;
+    private final DoubleSubscriber m_kIRotationSub;
+    private final DoubleSubscriber m_kDSpeedSub;
+    private final DoubleSubscriber m_kDRotationSub;
+    private final BooleanPublisher m_updatePIDPub;
+    private final BooleanSubscriber m_updatePIDSub;
+
     public AprilTagPID(DriveSubsystem driveSubsystem)
     {
         m_driveSubsystem = driveSubsystem;
 
-        SmartDashboard.putNumber("AprilTag/kP Speed", kSpeedKp);
-        SmartDashboard.putNumber("AprilTag/kP Rotation", kRotationKp);
-        SmartDashboard.putNumber("AprilTag/kI Speed", kSpeedKi);
-        SmartDashboard.putNumber("AprilTag/kI Rotation", kRotationKi);
-        SmartDashboard.putNumber("AprilTag/kD Speed", kSpeedKd);
-        SmartDashboard.putNumber("AprilTag/kD Rotation", kRotationKd);
-        SmartDashboard.putBoolean("AprilTag/Update PIDs", false);
-        
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        NetworkTable datatable = inst.getTable("AprilTagPID");
+
+        var kPSpeedSub = datatable.getDoubleTopic("P Speed");
+        kPSpeedSub.publish().set(kSpeedKp);
+        m_kPSpeedSub = kPSpeedSub.subscribe(kSpeedKp);
+
+        var kPRotationSub = datatable.getDoubleTopic("P Rotation");
+        kPRotationSub.publish().set(kRotationKp);
+        m_kPRotationSub = kPRotationSub.subscribe(kRotationKp);
+
+        var kISpeedSub = datatable.getDoubleTopic("I Speed");
+        kISpeedSub.publish().set(kSpeedKi);
+        m_kISpeedSub = kISpeedSub.subscribe(kSpeedKi);
+
+        var kIRotationSub = datatable.getDoubleTopic("I Rotation");
+        kIRotationSub.publish().set(kRotationKi);
+        m_kIRotationSub = kIRotationSub.subscribe(kRotationKi);
+
+        var kDSpeedSub = datatable.getDoubleTopic("D Speed");
+        kDSpeedSub.publish().set(kSpeedKd);
+        m_kDSpeedSub = kDSpeedSub.subscribe(kSpeedKd);
+
+        var kDRotationSub = datatable.getDoubleTopic("D Rotation");
+        kDRotationSub.publish().set(kRotationKd);
+        m_kDRotationSub = kDRotationSub.subscribe(kRotationKd);
+
+        var updatePIDSub = datatable.getBooleanTopic("update PID");
+        m_updatePIDPub = updatePIDSub.publish();
+        m_updatePIDPub.set(false);
+        m_updatePIDSub = updatePIDSub.subscribe(false);        
     }
 
     public void updateAprilTagSmartDashboard()
     {
-        boolean updatePID = SmartDashboard.getBoolean("AprilTag/Update PIDs", false);
-        if ( updatePID == true)
+
+        if (m_updatePIDSub.get())
          {
              updatePID();
-            SmartDashboard.putBoolean("AprilTag/Update PIDs", false);
+             m_updatePIDPub.set(false);
          }
     }   
 
     private void updatePID()
     {
-        kSpeedKp = SmartDashboard.getNumber("AprilTag/kP Speed", kSpeedKp);
-        kRotationKp = SmartDashboard.getNumber("AprilTag/kP Rotation", kRotationKp);
-        kSpeedKi = SmartDashboard.getNumber("AprilTag/kI Speed", kSpeedKi);
-        kRotationKi = SmartDashboard.getNumber("AprilTag/kI Rotation", kRotationKi);
-        kSpeedKd = SmartDashboard.getNumber("AprilTag/kD Speed", kSpeedKd);
-        kRotationKd = SmartDashboard.getNumber("AprilTag/kD Rotation", kRotationKd);
+        kSpeedKp = m_kPSpeedSub.get();
+        kRotationKp = m_kPRotationSub.get();
+        kSpeedKi = m_kISpeedSub.get();
+        kRotationKi = m_kIRotationSub.get();
+        kSpeedKd = m_kDSpeedSub.get();
+        kRotationKd = m_kDRotationSub.get();
     }
 
     public void setTargetPosition(double xPosition, double zPosition, double rotation)
